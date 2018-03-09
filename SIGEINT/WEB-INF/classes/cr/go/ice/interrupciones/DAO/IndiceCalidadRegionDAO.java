1a9ff253c00be54838e3905702efd4c5d0946fd8
Car11 <carlos.echc11@gmail.com>
2018-03-09 01:04:06 -0600

    SIGET WEBINF

--- SIGEINT/WEB-INF/classes/cr/go/ice/interrupciones/DAO/IndiceCalidadRegionDAO.java
+++
@@ -1,9 +0,0 @@
-package cr.go.ice.interrupciones.DAO;
-
-import java.util.List;
-
-import cr.go.ice.interrupciones.domain.IndiceCalidadRegion;
-
-public interface IndiceCalidadRegionDAO {
-	public List<IndiceCalidadRegion> buscar(Integer anno, Integer mes);
-}
